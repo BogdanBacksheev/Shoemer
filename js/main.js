9bb18d52777288a4e82ac068ac2e7a54d7c5227f
Bogdan <q51154612@gmail.com>
2018-04-03 00:43:46 +0300

    second commit

---
+++ js/main.js
@@ -0,0 +1,14 @@
+/*
+
+$(function() {
+	let windowWidth = $(window).width();
+
+	if (windowWidth>=992) {
+		$(".topMenu").appendTo(".header__topInner");
+		$(".search").appendTo(".header__topInner");
+		$(".mainMenu").appendTo(".header__bottomLeft");
+		$(".bascet").appendTo(".header__bottomInner");
+	}
+});
+
+*/
