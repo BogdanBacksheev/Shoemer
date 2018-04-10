6bf08e83785f5a112dc46dd2e17e10840d4a90ed
Bogdan <q51154612@gmail.com>
2018-04-11 01:10:55 +0300

    sixth commit

--- js/main.js
+++ js/main.js
@@ -10,9 +10,40 @@
 
 });*/
 
+$(function() {
+
+		$(".toggleMobMenu").on("click",function(){
+
+	let windowWidth = $(window).width();
+
+	if (windowWidth>=992) {
+
+		$(".topMenu").appendTo(".header__topInner");
+		$(".search").appendTo(".header__topInner");
+		$(".mainMenu").appendTo(".header__bottomLeft");
+		$(".basket").appendTo(".header__bottomInner");
+	}
+});
+});
+
+/*
 $(function(){
 	$(".toggleMobMenu").on("click",function(){
 		$(".header__mobMenuInner").toggleClass("open");
 	});
 
+});
+*/
+
+$(function(){
+	$(".accordion__itemTitle").on("click",function(){
+
+		var accordionItem = $(this).closest(".accordion__item");
+		var accordionItemSiblings = $(accordionItem).siblings(".accordion__item");
+		var accordionCheckboxWrap = $(accordionItemSiblings).children(".accordion__checkboxWrap");
+
+		$(accordionCheckboxWrap).hide();
+
+		$(this).siblings(".accordion__checkboxWrap").slideToggle();
+	});
 });
